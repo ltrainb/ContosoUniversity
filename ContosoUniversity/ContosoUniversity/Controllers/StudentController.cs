557e65120a429a46535df077de33a85b3de56458
Lionell Brown <ltrainb@gmail.com>
2014-07-07 16:51:14 -0400

    logging
    started logging.

--- ContosoUniversity/ContosoUniversity/Controllers/StudentController.cs
+++ ContosoUniversity/ContosoUniversity/Controllers/StudentController.cs
@@ -9,6 +9,7 @@ using System.Web.Mvc;
 using ContosoUniversity.DAL;
 using ContosoUniversity.Models;
 using PagedList;
+using System.Data.Entity.Infrastructure;
 
 namespace ContosoUniversity.Controllers
 {
@@ -96,7 +97,7 @@ namespace ContosoUniversity.Controllers
                     return RedirectToAction("Index");
                 }
             }
-            catch (DataException /* dex */)
+            catch (RetryLimitExceededException /* dex */)
             {
                 //log the error (uncomment dex variable and add a line here to write a log.
                 ModelState.AddModelError("", "unable to save changes. try again, and if problem persist, contack your adminstrator.");
@@ -135,7 +136,7 @@ namespace ContosoUniversity.Controllers
                     return RedirectToAction("Index");
                 }
             }
-            catch (DataException /* dex */)
+            catch (RetryLimitExceededException /* dex */)
             {
                 //log the error (uncomment dex variable and add a line here to write a log.
                 ModelState.AddModelError("", "unable to save changes. try again, and if problem persist, contack your adminstrator.");
@@ -173,7 +174,7 @@ namespace ContosoUniversity.Controllers
                 db.Students.Remove(student);
                 db.SaveChanges();
             }
-            catch (DataException /* dex */)
+            catch (RetryLimitExceededException /* dex */)
             {
                 //log the error
                 return RedirectToAction("Delete", new { id = id, saveChangesError = true });
---
+++ ContosoUniversity/ContosoUniversity/DAL/SchoolInterceptorLogging.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure.Interception;
+using System.Data.Entity.SqlServer;
+using System.Diagnostics;
+using System.Reflection;
+using ContosoUniversity.Logging;
+
+namespace ContosoUniversity.DAL
+{
+    public class SchoolInterceptorLogging : DbCommandInterceptor
+    {
+        private ILogger _logger = new Logger();
+        private readonly Stopwatch _stopwatch=new Stopwatch();
+
+        public override void ScalarExecuted(DbCommand command, DbCommandInterceptionContext<object> interceptionContext)
+        {
+            base.ScalarExecuted(command, interceptionContext);
+            _stopwatch.Restart();
+        }
+        public override void ScalarExecuting(DbCommand command, DbCommandInterceptionContext<object> interceptionContext)
+        {
+            _stopwatch.Stop();
+            if (interceptionContext.Exception != null)
+            {
+
+            }
+        }
+    }
+}
