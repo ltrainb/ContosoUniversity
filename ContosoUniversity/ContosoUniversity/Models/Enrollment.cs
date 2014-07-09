0068ac18129a8f657aea19e490870b71ba11675f
Lionell Brown <ltrainb@gmail.com>
2014-07-09 11:01:34 -0400

    added Instructors and Deprtments
    added them to database.

--- ContosoUniversity/ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/ContosoUniversity/Models/Enrollment.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
 namespace ContosoUniversity.Models
 {
     public enum Grade
@@ -10,6 +10,7 @@ namespace ContosoUniversity.Models
         public int EnrollmentID { get; set; }
         public int CourseID{get; set;}
         public int StudentID {get; set;}
+        [DisplayFormat(NullDisplayText="No Grade")]
         public Grade? Grade{get; set;}
         public virtual Course Course{get; set;}
         public virtual Student Student{get; set;}
